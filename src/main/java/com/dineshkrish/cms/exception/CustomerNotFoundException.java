4b2f8b95e6d9d6d9647d9e0a5581156232678cee
grozner <grozner@yahoo.com>
2021-12-29 08:16:33 -0500

    Delete src/main/java/com/dineshkrish/cms/exception directory

--- src/main/java/com/dineshkrish/cms/exception/CustomerNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.dineshkrish.cms.exception;
-
-public class CustomerNotFoundException extends RuntimeException {
-
-    public CustomerNotFoundException(String message) {
-        super(message);
-    }
-}
